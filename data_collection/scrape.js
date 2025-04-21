1b9d5782261ea282e0460056c149511760da4f4e
mKO9876 <martakokic98@gmail.com>
2025-04-21 19:37:39 +0200

    Added data_collection folder
    Put new folder for js files that collect data

--- data_collection/scrape.js
+++ data_collection/scrape.js
@@ -11,7 +11,7 @@ const { onRequest, onRequestFinished } = require('./pageHandlers');
 const autoScroll = require('./autoScroll');
 const { saveData, saveCSV } = require('./dataSaver');
 
-const OUTPUT_DIR = 'data';
+const OUTPUT_DIR = path.resolve(__dirname, '../data');
 const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36';
 
 (async () => {
